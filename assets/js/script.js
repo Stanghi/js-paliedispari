cd413a913135add7d9e656ca02b14019b4cb8f19
Stanghi <stangherlin.matti@gmail.com>
2022-10-19 17:39:56 +0200

    [add] even and odd

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ Palindroma:
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+console.log("---Palindroma---");
 
-const parolaUtente = prompt('Inserire una parola');
+const parolaUtente = prompt('Inserire una parola per vedere se è palindroma');
 console.log("Parola inserita: ", parolaUtente);
 metodo1(parolaUtente);
 metodo2(parolaUtente);
@@ -45,5 +46,75 @@ function metodo2(){
     function invertiParola(parolaUtente){
         let parolaUtenteInversa = parolaUtente.split('').reverse().join('');
         return parolaUtenteInversa;
+    }
+}
+
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+console.log("---PARI E DISPARI---");
+
+const userChoice = prompt("Scegli pari o dispari");
+const userNumber = parseInt(prompt("Inserisci un numero compreso tra 1 e 5"));
+
+if (userChoice.toLowerCase() === "pari" && userNumber >= 1 && userNumber <= 5){
+    choice();
+    randomForPc();
+}
+else if (userChoice.toLowerCase() === "dispari" && userNumber >= 1 && userNumber <= 5){
+    choice();
+    randomForPc();
+}
+else{
+    console.log("Inserimento non valido. Specificare pari/dispari e numero compreso tra 1 e 5");
+}
+
+
+function choice(){
+    console.log("Utente scommette: ",userChoice);
+    if (userChoice === "dispari"){
+        console.log("PC scommette: pari");
+    }
+    else {
+        console.log("PC scommette: dispari");
+    }
+    console.log("Numero utente: ",userNumber);
+}
+
+function randomForPc(){
+    const pcNumber = Math.floor(Math.random() * 5) + 1;
+    console.log("Numero pc: ",pcNumber);
+
+    const sum = userNumber + pcNumber;
+
+    winnerCalculator(sum);
+}
+
+function winnerCalculator(sum) {
+    if (!(sum % 2)){
+        console.log("la somma è pari");
+        const sumWinnerOutput = "pari";
+
+        checkWinner(sumWinnerOutput);
+    }
+    else{
+        console.log("la somma è dispari");
+        const sumWinnerOutput = "dispari";
+
+        checkWinner(sumWinnerOutput);
+    }
+}
+
+function checkWinner(sumWinnerOutput){
+    if (sumWinnerOutput === userChoice){
+        console.log("USER VINCE");
+    }
+    else {
+        console.log("PC VINCE");
     }
 }
